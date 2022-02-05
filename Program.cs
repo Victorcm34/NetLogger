e98fdd247fea93faf3b7efbc465cd9b2b057d3b5
Victor Mendez <victorcm34@gmail.com>
2022-02-05 22:17:20 +0100

    initial

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using NetLogger;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
